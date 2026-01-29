aa615fef5ea55c480147adc97742cd85dcdab4a7
MUJAHIDmd <muzammilk8035@gmail.com>
2026-01-29 15:00:55 +0500

    ok"

---
+++ Javascript Function/main.js
@@ -0,0 +1,35 @@
+// function sayHello() {
+//   console.log("Hello JavaScript");
+// }
+
+// sayHello();
+
+
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(5, 10);
+
+
+
+
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// let result = multiply(4, 3);
+// console.log(result);
+
+
+
+function calculator(a, b, operator) {
+  if (operator === "+") return a + b;
+  if (operator === "-") return a - b;
+  if (operator === "*") return a * b;
+  if (operator === "/") return a / b;
+}
+
+console.log(calculator(10, 5, "/"));
+
---
+++ Practice Question Task/script.js
@@ -0,0 +1,74 @@
+
+// VARIABLES
+// var name = "Muzammil";
+// var age = 20;
+// var city = "Karachi";
+
+// console.log("Name:", name);
+// console.log("Age:", age);
+// console.log("City:", city);
+
+
+
+// // DATA TYPES
+// var text = "Hello";
+// var number = 10;
+// var isStudent = true;
+
+
+// console.log(typeof text);       
+// console.log(typeof number);     
+// console.log(typeof isStudent);  
+
+
+
+// // BUILT-IN FUNCTIONS
+var userName = prompt("Enter your name:");
+alert("Welcome " + userName);
+console.log("Name length:", userName.length);
+
+var n1 = parseInt(prompt("Enter first number:"));
+var n2 = parseInt(prompt("Enter second number:"));
+console.log("Maximum number:", Math.max(n1, n2));
+
+
+
+// // ARITHMETIC OPERATORS
+// console.log("Addition:", n1 + n2);
+// console.log("Subtraction:", n1 - n2);
+// console.log("Multiplication:", n1 * n2);
+// console.log("Division:", n1 / n2);
+
+// // Area of rectangle
+// var width = 5;
+// var length = 10;
+// console.log("Area of Rectangle:", length * width);
+
+
+
+// // CONDITIONAL STATEMENTS
+
+// var marks = parseInt(prompt("Enter your marks:"));
+// if (marks >= 40) {
+//   console.log("Result: Pass");
+// } else {
+//   console.log("Result: Fail");
+// }
+
+// // Even / Odd
+// var num = parseInt(prompt("Enter a number:"));
+
+// if (num % 2 === 0) {
+//   console.log("Even Number");
+// } else {
+//   console.log("Odd Number");
+// }
+
+// // Vote check
+// var voteAge = parseInt(prompt("Enter your age:"));
+
+// if (voteAge >= 18) {
+//   console.log("Eligible for vote");
+// } else {
+//   console.log("Not eligible for vote");
+// }
